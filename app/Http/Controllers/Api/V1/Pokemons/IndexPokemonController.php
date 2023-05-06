619434767a5478c3f0b4dd40fcb0e9f925a29eb3
zenofro <zeno@bmksolutions.be>
2023-05-06 20:06:11 +0200

    index api sort parameter

--- app/Http/Controllers/Api/V1/Pokemons/IndexPokemonController.php
+++ app/Http/Controllers/Api/V1/Pokemons/IndexPokemonController.php
@@ -4,19 +4,34 @@ namespace App\Http\Controllers\Api\V1\Pokemons;
 
 use App\Http\Resources\PokemonCollection;
 use App\Models\Pokemon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class IndexPokemonController
 {
-    public function __invoke()
+    use ValidatesRequests;
+
+    public function __invoke(Request $request)
     {
-        return new PokemonCollection(
-            Pokemon::with([
-                'sprite',
-                'abilities',
-                'moves.versions',
-                'stats',
-                'types',
-            ])->get()
-        );
+        $this->validate($request, [
+            'sort' => ['nullable', Rule::in(['name-asc', 'name-desc', 'id-asc', 'id-desc'])]
+        ]);
+
+        [$sortColumn, $sortDirection] = explode('-', $request->input('sort'));
+
+        $pokemons = Pokemon::with([
+            'sprite',
+            'abilities',
+            'moves.versions',
+            'stats',
+            'types',
+        ])->when(
+            $request->has('sort'),
+            fn(Builder $query) => $query->orderByRaw("{$sortColumn} {$sortDirection}")
+        )->get();
+
+        return new PokemonCollection($pokemons);
     }
 }
