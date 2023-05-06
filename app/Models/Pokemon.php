1973d43ec8f40923ecd4c7675760d884b47119ed
zenofro <zeno@bmksolutions.be>
2023-05-06 11:08:33 +0200

    sprite model + migration

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pokemon extends Model
 {
     protected $guarded = ['id'];
+
+    public function sprites(): HasMany
+    {
+        return $this->hasMany(Sprite::class);
+    }
 }
---
+++ database/migrations/2023_05_06_110406_create_sprites_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Pokemon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('sprites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Pokemon::class)->constrained()->cascadeOnDelete();
+            $table->string('front_default');
+            $table->string('front_shiny');
+            $table->string('front_female');
+            $table->string('front_shiny_female');
+            $table->string('back_default');
+            $table->string('back_shiny');
+            $table->string('back_female');
+            $table->string('back_shiny_female');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('sprites');
+    }
+};
